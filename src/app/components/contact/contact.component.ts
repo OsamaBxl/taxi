417f5415246aaf838573c5e99f604df3bffb77c3
OsamaBxl <86070580+OsamaBxl@users.noreply.github.com>
2021-12-23 12:32:59 +0100

    contact page ts file modified

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -4,32 +4,31 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent implements OnInit {
+  contactUsForm: FormGroup;
 
-  contactUsForm:FormGroup;
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-
     this.contactUsForm = new FormGroup({
-      'name' : new FormControl(null , Validators.required),
-      'email' : new FormControl(null , Validators.required),
-      'message': new FormControl(null , Validators.required)
+      name: new FormControl(null, Validators.required),
+      email: new FormControl(null, Validators.required),
+      phone: new FormControl(null, Validators.required),
+      subject: new FormControl(null, Validators.required),
+      message: new FormControl(null, Validators.required),
     });
   }
 
-
-
-  sendMail(){
-    const message={
-      name:this.contactUsForm.get(['name'])?.value,
-      email:this.contactUsForm.get(['email'])?.value,
-      message:this.contactUsForm.get(['message'])?.value,
-    }
+  sendMail() {
+    const message = {
+      name: this.contactUsForm.get(['name'])?.value,
+      email: this.contactUsForm.get(['email'])?.value,
+      phone: this.contactUsForm.get(['phone'])?.value,
+      subject: this.contactUsForm.get(['subject'])?.value,
+      message: this.contactUsForm.get(['message'])?.value,
+    };
     console.log(message);
-    
   }
 }
